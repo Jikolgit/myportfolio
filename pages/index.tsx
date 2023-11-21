16e3d3948567837afeca4265aae27af81eaf6c4c
abdel <zbio234@gmail.com>
2023-11-21 21:10:30 +0100

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Abdel BIO</title>
         <meta name="description" content="Abdel Bio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
